3b95ed871d52d1b36d9ed70683504f98e308b15b
mccambria <matthew.carl.cambria@gmail.com>
2025-08-02 21:27:06 -0700

    Test from nv desktop

---
+++ hardware/tisaph.py
@@ -0,0 +1,11 @@
+import pylablib as pll
+from pylablib.devices import M2
+
+"""
+laser = M2.Solstis("192.168.0.195", 62566, use_websocket=True)
+print(laser.get_system_status())
+print(laser.is_wavemeter_connected())
+# laser.connect_wavemeter()
+print(laser.get_coarse_wavelength())
+laser.close()
+"""
